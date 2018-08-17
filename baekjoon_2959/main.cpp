1885dd4a507492097582a5f53ae822e19a772909
WaniFi <skelpd24@gmail.com>
2018-08-17 17:47:53 +0900

    2959 문제 풀이 완료

---
+++ baekjoon_2959/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n[4];
+
+int main() {
+	for (int i = 0; i < 4; i++) cin >> n[i];
+	sort(n, n+4);
+	cout << n[0] * n[2];
+	return 0;
+}
